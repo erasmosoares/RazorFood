94a82250f00289d5f1e118a3e043d0b665c275a0
Erasmo Soares <erasmosaraujo@gmail.com>
2019-04-02 22:36:27 -0300

    Initial Commit

---
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration config;
+
+        public string Message { get; set; }
+
+        public IRestaurantData RestaurantData { get; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        public ListModel(IConfiguration config,
+                         IRestaurantData restaurantData)
+        {
+            this.config = config;
+            RestaurantData = restaurantData;
+        }
+
+        public void OnGet()
+        {
+            Message = config["Message"];
+            Restaurants = RestaurantData.GetAll();
+        }
+    }
+}
